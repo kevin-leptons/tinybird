9c0be9c74540cdd32ad926092353c1db745095df
Kevin Leptons <kevin.leptons@gmail.com>
2018-12-04 22:37:55 +0700

    add conf table

---
+++ test/conf_table.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+
+const {ConfTable} = require('logcore/service')
+
+const {service} = require('./lib')
+
+describe('class ConfTable', () => {
+    let conf_table = service.get(ConfTable)
+    let key1 = 'key1'
+    let key2 = 'key2'
+    let value1 = 'value1'
+    let value2 = 'value2'
+
+    it('set()', async () => {
+        await conf_table.set(key1, value1)
+        await conf_table.set(key2, value2)
+    })
+
+    it('list()', async () => {
+        let conf = await conf_table.list()
+        assert.equal(conf.get(key1), value1)
+        assert.equal(conf.get(key2), value2)
+    })
+})
