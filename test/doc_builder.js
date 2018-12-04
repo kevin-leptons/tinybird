5fde56562d1c1bd2361154369186651fb74d653a
Kevin Leptons <kevin.leptons@gmail.com>
2018-12-04 23:00:40 +0700

    add doc builder skeleton

---
+++ test/doc_builder.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+const {DocBuilder} = require('logcore/service')
+
+const {service} = require('./lib')
+
+describe('class DocBuilder', () => {
+    let doc_builder = service.get(DocBuilder)
+
+    it('build a document', () => {
+        let example_dir = path.join(__dirname, 'example')
+        let root = path.join(example_dir, 'root')
+        let doc_dir = path.join(example_dir, 'other')
+        let doc_cpp = path.join(example_dir, 'other_cpp')
+        let doc_python = path.join(example_dir, 'other_python')
+        let dest = path.join(__dirname, '../dest')
+
+        doc_builder.set_root(root)
+        doc_builder.add_many(doc_dir)
+        doc_builder.add(doc_cpp)
+        doc_builder.add(doc_python)
+        doc_builder.write(dest)
+    })
+})
