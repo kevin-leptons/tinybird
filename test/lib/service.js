7e2e428d9689ef4ee2b6bec54a51dc9874ecf02f
Kevin Leptons <kevin.leptons@gmail.com>
2018-12-04 22:44:31 +0700

    test with memory db

--- test/lib/service.js
+++ test/lib/service.js
@@ -1,8 +1,7 @@
-const path = require('path')
 const {Service} = require('logcore/service')
 
 let service = new Service({
-    storage_file: path.join(__dirname, '../../tmp/db.sqlite')
+    storage_file: ':memory:'
 })
 
 module.exports = service
